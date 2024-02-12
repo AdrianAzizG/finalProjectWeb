ad78e4be7cef1da3a660d7a72aafcb53d7b7ba6b
aziz_ <azizadrian9@gmail.com>
2024-02-12 13:17:19 +0700

    Create Case Register 2 and 3

---
+++ Object Repository/Record/Register/Page_Advantage Shopping/label_REGISTER_center block smollMargin.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_REGISTER_center block smollMargin</name>
+   <tag></tag>
+   <elementGuidId>1779195e-2046-4eca-95b6-86d39058b826</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//section[@id='registerPage']/article/sec-form/div[2]/label</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>label.center.block.smollMargin</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>label</value>
+      <webElementGuid>93b33a75-4401-48f6-9c95-a8bc7686a5e0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>center block smollMargin</value>
+      <webElementGuid>3c52278e-d14e-43b7-9629-7a88884fb504</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-ng-show</name>
+      <type>Main</type>
+      <value>!registerSuccess</value>
+      <webElementGuid>6dda02ad-6c49-4ee6-b633-e86793b4ec6b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;registerPage&quot;)/article[1]/sec-form[@class=&quot;ng-isolate-scope secForm&quot;]/div[@class=&quot;center&quot;]/label[@class=&quot;center block smollMargin&quot;]</value>
+      <webElementGuid>2ff09c49-5d34-464a-a369-f723fc0c7371</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//section[@id='registerPage']/article/sec-form/div[2]/label</value>
+      <webElementGuid>bd66aa26-d56a-496a-9ba1-7c0496a9688e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='REGISTER'])[1]/following::label[1]</value>
+      <webElementGuid>8e7e1c7b-60a3-43ed-ab93-4722c7bea8e1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='I agree to the www.AdvantageOnlineShopping.com Conditions of Use and Privacy Notice'])[2]/following::label[1]</value>
+      <webElementGuid>69191aad-e4b3-4490-b699-7f8e85197ee7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='ALREADY HAVE AN ACCOUNT?'])[1]/preceding::label[1]</value>
+      <webElementGuid>805f0ffc-9d68-41e0-933e-d278ac5fd38c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Hi ,'])[1]/preceding::label[2]</value>
+      <webElementGuid>2a7a6952-09d6-44b3-aab1-81bd22a6481c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/label</value>
+      <webElementGuid>55096a8d-4084-404f-9077-037f8d5b207b</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Record/Register/Page_Advantage Shopping/label_User name already exists.rs
@@ -0,0 +1,129 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_User name already exists</name>
+   <tag></tag>
+   <elementGuidId>0293eae1-9966-4eba-abc0-0604a22b9a6c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//section[@id='registerPage']/article/sec-form/div[2]/label</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>label.invalid.center.block.smollMargin</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>label</value>
+      <webElementGuid>51434ce8-5d43-4937-b312-fc72e16d37b4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>invalid center block smollMargin</value>
+      <webElementGuid>da8f32a9-9f21-444d-ae97-d76aefd200ea</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-ng-show</name>
+      <type>Main</type>
+      <value>!registerSuccess</value>
+      <webElementGuid>49d03693-ad5c-488c-a0c7-fcbfc1de2e30</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                    User name already exists
+                </value>
+      <webElementGuid>2e9fd29b-ac99-4daa-9387-d3389d297b40</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;registerPage&quot;)/article[1]/sec-form[@class=&quot;ng-isolate-scope secForm&quot;]/div[@class=&quot;center&quot;]/label[@class=&quot;invalid center block smollMargin&quot;]</value>
+      <webElementGuid>34b8a179-3b9e-432e-acfa-617bed8a5142</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//section[@id='registerPage']/article/sec-form/div[2]/label</value>
+      <webElementGuid>587971ab-edda-4e85-af39-767764ded9b1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='REGISTER'])[1]/following::label[1]</value>
+      <webElementGuid>acc1488a-cc07-4cae-8178-9a5f40a08a37</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='I agree to the www.AdvantageOnlineShopping.com Conditions of Use and Privacy Notice'])[2]/following::label[1]</value>
+      <webElementGuid>b1453e05-95b6-458e-bc65-969c89135e18</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='ALREADY HAVE AN ACCOUNT?'])[1]/preceding::label[1]</value>
+      <webElementGuid>a695480e-45da-4254-9f3b-68132ec47a3c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Hi ,'])[1]/preceding::label[2]</value>
+      <webElementGuid>50e7bd13-74e3-4c3d-85e9-1c37b558d7ae</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='User name already exists']/parent::*</value>
+      <webElementGuid>e008e715-98e3-4a99-8b71-ab97218c4c2d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/label</value>
+      <webElementGuid>699dbb29-665e-4e92-82db-d7958566f6e4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//label[(text() = '
+                    User name already exists
+                ' or . = '
+                    User name already exists
+                ')]</value>
+      <webElementGuid>80307064-bb44-4c83-b005-d70373778d37</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
