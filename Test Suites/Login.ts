0bba908409883aeeaa2b6bd67c27d5acc45584c8
aziz_ <azizadrian9@gmail.com>
2024-02-13 07:33:24 +0700

    Create Test Suite for Login

---
+++ Test Suites/Login.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d314ccfb-a8c5-439c-abfd-b8892d466ee0</testSuiteGuid>
+   <testCaseLink>
+      <guid>d52fc168-1ec8-4ca4-8e8b-6388d7023eac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login 01 - Sukses</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>2c0d889b-076f-46fc-9b49-50208e819ff6</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Users</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>2c0d889b-076f-46fc-9b49-50208e819ff6</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>df1fcae6-306e-442c-9951-fdebbb18b555</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>2c0d889b-076f-46fc-9b49-50208e819ff6</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>7405bf5e-ddb9-4adb-95d8-7b98d81fea62</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c382de68-7245-4a3f-a031-4a076391a434</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login 02 - Gagal</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>e03ff8af-7a83-4dc6-af7f-9239a95f51e9</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a26891f1-83a0-4c5b-80f1-a72a4d314efa</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
